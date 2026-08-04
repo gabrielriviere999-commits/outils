b7e312b906011fbf39cb389934bdb5fa12816cec
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-08-04 13:35:25 +0400

    Add files via upload

--- multitouch.js
+++ multitouch.js
@@ -1,30 +1,23 @@
 function addMultiTouch(selector){
     var els = document.querySelectorAll(selector);
-
     for(var i=0; i<els.length; i++){
         var el = els[i];
-
         var action = el.getAttribute("data-action");
         if(action){
             el._action = window[action];
         }
-
         el.onpointerdown = function(e){
             this._touchTriggered = (e.pointerType === "touch");
         };
-
         el.onpointerup = function(e){
             if(this._touchTriggered){
-
                 var tag = this.tagName.toLowerCase();
-
                 // Cas spécial : summary → toggle manuel
                 if(tag === "summary"){
                     var d = this.parentNode;
                     d.open = !d.open;
                     return;
                 }
-
                 // Cas spécial : input
                 if(tag === "input"){
                     var type = (this.type || "").toLowerCase();
@@ -47,20 +40,19 @@ function addMultiTouch(selector){
                         return;
                     }
                     // Autres input (text, number, etc.)
+                    if(document.activeElement===this){this.blur();this.focus();}else{this.focus();}
                     this.focus();
                     if(this._action){
                         this._action();
                     }
                     return;
                 }
-
                 // Cas normal → action tactile
                 if(this._action){
                     this._action();
                 }
             }
         };
-
         el.onclick = function(e){
             if(this._touchTriggered){
                 if(this.tagName.toLowerCase() === "input"){
