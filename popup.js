f6c3da7a934e045d88a3d8a1e65704ee134be4ee
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-09-08 10:14:16 +0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -85,8 +85,8 @@ function closePopup() {
 }
 document.addEventListener("keydown", function(e){
     e = e || window.event;
-    if (e.key === "Enter" || e.keyCode === 13 ||
-        e.key === "Escape" || e.keyCode === 27) {
+    if ( e.keyCode === 13 ||
+         e.keyCode === 27) {
         // 1. Le color picker est au-dessus du popup
         var colorPicker = document.querySelector('.overlayColorPicker');
         if (colorPicker && colorPicker.style.display !== "none") {
