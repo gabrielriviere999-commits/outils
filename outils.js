ae6e095f0515f70f22a486c8e82ceb2d52608d66
gabrielriviere999-commits <gabriel.riviere999@gmail.com>
2026-09-11 14:46:42 +0400

    Add files via upload

--- outils.js
+++ outils.js
@@ -162,6 +162,7 @@ for (var i = 0; i < scrollButtons.length; i++) {
         btn.addEventListener("pointerdown", function(e) {
             e.preventDefault();
             scrolling = true;
+            btn.classList.add("active");
             if (btn.setPointerCapture) {
                 try {
                     btn.setPointerCapture(e.pointerId);
@@ -174,9 +175,11 @@ for (var i = 0; i < scrollButtons.length; i++) {
         });
         btn.addEventListener("pointercancel", function() {
             scrolling = false;
+            btn.classList.remove("active");
         });
         btn.addEventListener("lostpointercapture", function() {
             scrolling = false;
+            btn.classList.remove("active");
         });
         btn.addEventListener("pointerleave", function(e) {
             if (e.pointerType === "mouse") {
